1189a7faa219cb3d32e7b8798e62252a66ac83e1
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2022-11-08 21:09:38 +0200

    added toggle functionality

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -12,6 +12,7 @@ export class TaskItemComponent implements OnInit {
 
   @Input() task: Task = TASKS[0];
   @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
   faTimes = faTimes;
   constructor() { }
 
@@ -20,5 +21,8 @@ export class TaskItemComponent implements OnInit {
 
   onDelete(task: Task) {
     this.onDeleteTask.emit(task);
+  }
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task)
   }
 }
